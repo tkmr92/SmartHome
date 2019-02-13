641ffb79fb212986f3ce016fcfa836a4ad61e49d
tkmr <tkmr@kilo-tango.net>
2019-02-13 15:27:08 -0700

    made changes to be more jinja-template like

---
+++ commandcenter.py
@@ -0,0 +1,24 @@
+import flask
+app = flask.Flask(__name__)
+
+navbar = ['home', 'todo', 'schedule', 'shopping', 'change user']
+
+@app.route('/')
+def display_home(nav=navbar):
+    return flask.render_template('index.html', nav=nav)
+
+@app.route('/todo')
+def display_todo(nav=navbar):
+    return flask.render_template('todo.html', nav=nav)
+
+@app.route('/schedule')
+def display_schedule(nav=navbar):
+    return flask.render_template('schedule.html', nav=nav)
+
+@app.route('/shopping')
+def display_shopping(nav=navbar):
+    return flask.render_template('shopping.html', nav=nav)
+
+@app.route('/changeuser')
+def display_change_user(nav=navbar):
+    return flask.render_template('changeuser.html', nav=nav)
