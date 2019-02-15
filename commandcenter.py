7d077921930baade90dba2e6353f489127cd6bf2
tkmr <tkmr@kilo-tango.net>
2019-02-15 13:39:10 -0700

    basic logger functionality implemented

--- commandcenter.py
+++ commandcenter.py
@@ -1,23 +1,32 @@
 import flask
+import lib.Logger
+
+logger = lib.Logger.Logger(__name__, "debug")
 app = flask.Flask(__name__)
 
+logger.log("info", "flask started")
 navbar = ['home', 'todo', 'schedule', 'shopping', 'change user']
+
 
 @app.route('/')
 def display_home(nav=navbar):
     return flask.render_template('index.html', nav=nav)
 
+
 @app.route('/todo')
 def display_todo(nav=navbar):
     return flask.render_template('todo.html', nav=nav)
+
 
 @app.route('/schedule')
 def display_schedule(nav=navbar):
     return flask.render_template('schedule.html', nav=nav)
 
+
 @app.route('/shopping')
 def display_shopping(nav=navbar):
     return flask.render_template('shopping.html', nav=nav)
+
 
 @app.route('/changeuser')
 def display_change_user(nav=navbar):
---
+++ lib/Logger.py
@@ -0,0 +1,31 @@
+import logging
+
+
+class Logger:
+    def __init__(self, loggername, loglevel):
+        self.name = loggername
+        loglevel = getattr(logging, loglevel.upper(), None)
+        logging.basicConfig(format=f"%(asctime)s %(levelname)s: %(message)s", filename=f"{self.name!s}.log",
+                            level=loglevel, filemode="w", datefmt="%d/%m/%Y %H:%M:%S")
+
+    def log(self, level, message):
+        level = level.upper()
+        logging.debug("Log function called in Logger")
+        if level == "DEBUG":
+            logging.debug(f"From: {self.name!s}: {message!s}")
+            return
+        if level == "INFO":
+            logging.info(f"From: {self.name!s}: {message!s}")
+            return
+        if level == "WARNING":
+            logging.warning(f"From: {self.name!s}: {message!s}")
+            return
+        if level == "ERROR":
+            logging.error(f"From: {self.name!s}: {message!s}")
+            return
+        if level == "CRITICAL":
+            logging.critical(f"From: {self.name!s}: {message!s}")
+            return
+        else:
+            logging.warning(f"Unknown log level encountered. Level was {level!s}. Message was {message!s}")
+            return
